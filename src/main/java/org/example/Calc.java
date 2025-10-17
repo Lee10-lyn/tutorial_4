c5ffe8cc70ed18c21bbed1b334c92c3bd7a5d212
Lee-lyn <18810055299@163.com>
2025-10-17 11:53:37 +0800

    Fix subtract method: change return a + b to return a - b

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,14 +1,11 @@
 package org.example;
 
 public class Calc {
-
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    public int subtract(int a, int b) {
+        return a - b;  // 从错误的 return a + b; 修正为 return a - b;
     }
 }
